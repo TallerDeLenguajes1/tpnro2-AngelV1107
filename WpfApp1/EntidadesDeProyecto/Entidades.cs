4fa48d7ddfa63cb9e24f88ee1ffa9ad0ebc36bee
AngelV1107 <angelleandro@live.com>
2019-09-23 11:29:44 -0300

    Vista Principal
    Vista principal de la aplicacion con listBox principal, panel de informacion, comboBox para seleccionar entre lista de Alumnos, Empleados o Cursos, y botones correspondientes

---
+++ WpfApp1/EntidadesDeProyecto/Entidades.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntidadesDeProyecto
+{
+    public class Persona
+    {
+        string nombre;
+        string apellido;
+        DateTime fechaDeNacimiento;
+        long dni;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+
+            set
+            {
+                apellido = value;
+            }
+        }
+
+        public DateTime FechaDeNacimiento
+        {
+            get
+            {
+                return fechaDeNacimiento;
+            }
+
+            set
+            {
+                fechaDeNacimiento = value;
+            }
+        }
+
+        public long Dni
+        {
+            get
+            {
+                return dni;
+            }
+
+            set
+            {
+                dni = value;
+            }
+        }
+
+        public int Edad()
+        {
+
+            //Hacer Edad
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return dni + ", " + apellido + ", " + apellido + ", " + FechaDeNacimiento.ToShortDateString();
+        }
+
+    }
+
+    public class Alumno : Persona
+    {
+        List<Cuota> cuotas;
+
+        public Alumno()
+        {
+            cuotas = new List<Cuota>(12);
+        }
+
+        public List<Cuota> Cuotas
+        {
+            get
+            {
+                return cuotas;
+            }
+
+            set
+            {
+                cuotas = value;
+            }
+        }
+
+    }
+
+    public enum Cargo
+    {
+        docente,
+        director,
+        conserje
+    }
+
+
+    public class Empleado : Persona
+    {
+        DateTime fechaDeAlta;
+        Cargo cargoEmp;
+        decimal sueldo;
+
+        public DateTime FechaDeAlta
+        {
+            get
+            {
+                return fechaDeAlta;
+            }
+
+            set
+            {
+                fechaDeAlta = value;
+            }
+        }
+
+        public Cargo CargoEmp
+        {
+            get
+            {
+                return cargoEmp;
+            }
+
+            set
+            {
+                cargoEmp = value;
+            }
+        }
+
+        public decimal Sueldo
+        {
+            get
+            {
+                return sueldo;
+            }
+
+            set
+            {
+                sueldo = value;
+            }
+        }
+
+        public int Antiguedad()
+        {
+            //hacer antiguedad
+            return 0;
+        }
+
+    }
+
+    public abstract class Curso
+    {
+        DateTime turno;
+        List<Alumno> alumnos;
+        Empleado docente;
+        string tema;
+        float cuota;
+        float inscripcion;
+
+        public abstract string GetTipo();
+
+
+        public DateTime Turno
+        {
+            get
+            {
+                return turno;
+            }
+
+            set
+            {
+                turno = value;
+            }
+        }
+
+        public List<Alumno> Alumnos
+        {
+            get
+            {
+                return alumnos;
+            }
+
+            set
+            {
+                alumnos = value;
+            }
+        }
+
+        public Empleado Docente
+        {
+            get
+            {
+                return docente;
+            }
+
+            set
+            {
+                docente = value;
+            }
+        }
+
+        public string Tema
+        {
+            get
+            {
+                return tema;
+            }
+
+            set
+            {
+                tema = value;
+            }
+        }
+
+        public float Cuota
+        {
+            get
+            {
+                return cuota;
+            }
+
+            set
+            {
+                cuota = value;
+            }
+        }
+
+        public float Inscripcion
+        {
+            get
+            {
+                return inscripcion;
+            }
+
+            set
+            {
+                inscripcion = value;
+            }
+        }
+
+        public void CargarAlumno(Alumno alu)
+        {
+
+        }
+
+        public void GuardarDatosEnArchivo()
+        {
+
+        }
+
+        public virtual decimal ValorCuota()
+        {
+            return 0;
+        }
+
+
+    }
+
+    public class Presencial : Curso
+    {
+        public override string GetTipo()
+        {
+            return "Presencial";
+        }
+    }
+
+    public class SemiPresencial : Curso
+    {
+        public override string GetTipo()
+        {
+            return "SemiPresencial";
+        }
+    }
+
+    public class NoPresencial : Curso
+    {
+        public override string GetTipo()
+        {
+            return "No Presencial";
+        }
+    }
+
+    public class Cuota
+    {
+        decimal valor;
+        bool pagada;
+
+        public decimal Valor
+        {
+            get
+            {
+                return valor;
+            }
+
+            set
+            {
+                valor = value;
+            }
+        }
+
+        public bool Pagada
+        {
+            get
+            {
+                return pagada;
+            }
+
+            set
+            {
+                pagada = value;
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Vistas/MainWindow.xaml.cs
@@ -0,0 +1,273 @@
+﻿using EntidadesDeProyecto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+
+    enum Entidad
+    {
+        Alumno,
+        Empleado,
+        Curso
+    }
+    enum ModoABM
+    {
+        Alta,
+        Modif,
+        SinSelecc
+    }
+    public partial class MainWindow : Window
+    {
+        List<Alumno> alumnos;
+        List<Empleado> empleados;
+        List<Curso> cursos;
+
+        public MainWindow()
+        {
+            alumnos = new List<Alumno>();
+            empleados = new List<Empleado>();
+            cursos = new List<Curso>();
+
+            InitializeComponent();
+        }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void CbxEntidades_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.IsLoaded)
+            {
+                UpdateList();
+                UpdateInfo();
+                UpdateBtns();
+            }
+        }
+        private void UpdateInfo()
+        {
+            stkInfo.Children.Clear();
+            switch (EntidadSeleccionada())
+            {
+                case Entidad.Alumno:
+                    stkInfo.Children.Add(WPInfo("Nombre:", ""));
+                    stkInfo.Children.Add(WPInfo("Apellido:", ""));
+                    stkInfo.Children.Add(WPInfo("DNI:", ""));
+                    stkInfo.Children.Add(WPInfo("Fecha de nacimiento:", ""));
+                    stkInfo.Children.Add(WPInfo("Edad:", ""));
+                    stkInfo.Children.Add(WPInfo("Cuotas pagadas:", ""));
+                    break;
+                case Entidad.Empleado:
+                    stkInfo.Children.Add(WPInfo("Nombre:", ""));
+                    stkInfo.Children.Add(WPInfo("Apellido:", ""));
+                    stkInfo.Children.Add(WPInfo("DNI:", ""));
+                    stkInfo.Children.Add(WPInfo("Fecha de nacimiento:", ""));
+                    stkInfo.Children.Add(WPInfo("Edad:", ""));
+                    stkInfo.Children.Add(WPInfo("Fecha de alta:", ""));
+                    stkInfo.Children.Add(WPInfo("Antiguedad:", ""));
+                    stkInfo.Children.Add(WPInfo("Cargo:", ""));
+                    stkInfo.Children.Add(WPInfo("Sueldo:", ""));
+                    break;
+                case Entidad.Curso:
+                    stkInfo.Children.Add(WPInfo("Tema:", ""));
+                    stkInfo.Children.Add(WPInfo("Docente:", ""));
+                    stkInfo.Children.Add(WPInfo("Turno:", ""));
+                    stkInfo.Children.Add(WPInfo("Inscripcion:", ""));
+                    stkInfo.Children.Add(WPInfo("Cuota:", ""));
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        private void UpdateInfo(Alumno alum)
+        {
+            stkInfo.Children.Clear();
+            stkInfo.Children.Add(WPInfo("Nombre:", alum.Nombre));
+            stkInfo.Children.Add(WPInfo("Apellido:", alum.Apellido));
+            stkInfo.Children.Add(WPInfo("DNI:", alum.Dni.ToString()));
+            stkInfo.Children.Add(WPInfo("Fecha de nacimiento:", alum.FechaDeNacimiento.ToShortDateString()));
+            stkInfo.Children.Add(WPInfo("Edad:", alum.Edad().ToString()));
+            stkInfo.Children.Add(WPInfo("Cuotas pagadas:", ""));
+        }
+        private void UpdateInfo(Empleado emp)
+        {
+            stkInfo.Children.Clear();
+            stkInfo.Children.Add(WPInfo("Nombre:", emp.Nombre));
+            stkInfo.Children.Add(WPInfo("Apellido:", emp.Apellido));
+            stkInfo.Children.Add(WPInfo("DNI:", emp.Dni.ToString()));
+            stkInfo.Children.Add(WPInfo("Fecha de nacimiento:", emp.FechaDeNacimiento.ToShortDateString()));
+            stkInfo.Children.Add(WPInfo("Edad:", emp.Edad().ToString()));
+            stkInfo.Children.Add(WPInfo("Fecha de alta:", emp.FechaDeAlta.ToShortDateString()));
+            stkInfo.Children.Add(WPInfo("Antiguedad:", emp.Antiguedad().ToString()));
+            stkInfo.Children.Add(WPInfo("Cargo:", emp.CargoEmp.ToString()));
+            stkInfo.Children.Add(WPInfo("Sueldo:", emp.Sueldo.ToString()));
+        }
+        private void UpdateInfo(Curso cur)
+        {
+            stkInfo.Children.Clear();
+            stkInfo.Children.Add(WPInfo("Tema:", cur.Tema));
+            stkInfo.Children.Add(WPInfo("Docente:", cur.Docente.Apellido + ", " + cur.Docente.Nombre ));
+            stkInfo.Children.Add(WPInfo("Turno:", cur.Turno.ToString()));
+            stkInfo.Children.Add(WPInfo("Inscripcion:", cur.Inscripcion.ToString()));
+            stkInfo.Children.Add(WPInfo("Cuota:", cur.Cuota.ToString()));
+        }
+        private void UpdateList()
+        {
+            lbxEntidades.Items.Clear();
+
+            switch (EntidadSeleccionada())
+            {
+                case Entidad.Alumno:                    
+                    foreach(Alumno alum in alumnos)
+                    {
+                        lbxEntidades.Items.Add(alum);
+                    }
+                    break;
+                case Entidad.Empleado:
+                    foreach (Empleado emp in empleados)
+                    {
+                        lbxEntidades.Items.Add(emp);
+                    }
+                    break;
+                case Entidad.Curso:
+                    foreach (Curso cur in cursos)
+                    {
+                        lbxEntidades.Items.Add(cur);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            lbxEntidades.Items.Add("<nuevo>");
+        }
+        private void UpdateBtns()
+        {
+            if (DeterminarModoABM() != ModoABM.SinSelecc)
+            {
+                btnABM.IsEnabled = true;
+                btnOpcion1.IsEnabled = true;
+                btnOpcion2.IsEnabled = true;
+            }
+            else
+            {
+                btnABM.IsEnabled = false;
+                btnOpcion1.IsEnabled = false;
+                btnOpcion2.IsEnabled = false;
+            }
+
+            switch (EntidadSeleccionada())
+            {
+                case Entidad.Alumno:
+                    btnOpcion1.Content = "Pagar cuotas";
+                    btnOpcion2.Content = "";
+
+                    btnOpcion2.IsEnabled = false;
+                    break;
+                case Entidad.Empleado:
+                    btnOpcion1.Content = "";
+                    btnOpcion2.Content = "";
+
+                    btnOpcion1.IsEnabled = false;
+                    btnOpcion2.IsEnabled = false;
+                    break;
+                case Entidad.Curso:
+                    btnOpcion1.Content = "Ver inscripciones";
+                    btnOpcion2.Content = "Generar CSV";
+                    break;
+                default:
+                    break;
+            }
+        }
+        private WrapPanel WPInfo(string tipo, string contenido)
+        {
+            WrapPanel wp = new WrapPanel();
+            Label lbTipo = new Label();
+            Label lbInfo = new Label();
+
+            lbTipo.Content = tipo;
+            lbInfo.Content = contenido;
+
+            wp.Children.Add(lbTipo);
+            wp.Children.Add(lbInfo);
+
+            return wp;
+        }
+        private Entidad EntidadSeleccionada()
+        {
+            Entidad ent = (Entidad)cbxEntidades.SelectedIndex;
+            return ent;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cbxEntidades.SelectedIndex = (int) Entidad.Alumno;
+            UpdateList();
+            UpdateInfo();
+            UpdateBtns();
+        }
+        private void LbxEntidades_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DeterminarModoABM() != ModoABM.SinSelecc)
+            {                
+                if (DeterminarModoABM() == ModoABM.Alta)
+                {
+                    UpdateInfo();
+                }
+                else
+                {
+                    switch (EntidadSeleccionada())
+                    {
+                        case Entidad.Alumno:
+                            UpdateInfo(alumnos[lbxEntidades.SelectedIndex]);
+                            break;
+                        case Entidad.Empleado:
+                            UpdateInfo(empleados[lbxEntidades.SelectedIndex]);
+                            break;
+                        case Entidad.Curso:
+                            UpdateInfo(cursos[lbxEntidades.SelectedIndex]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            UpdateBtns();
+        }
+        private ModoABM DeterminarModoABM()
+        {
+            if (lbxEntidades.SelectedIndex != -1)
+            {
+                if (lbxEntidades.SelectedItem.ToString() == "<nuevo>")
+                {
+                    return ModoABM.Alta;
+                }
+                else
+                {
+                    return ModoABM.Modif;
+                }
+            }
+            else
+            {
+                return ModoABM.SinSelecc;
+            }
+        }
+    }
+}
